6044e581f0baee7b20f984262dab0e97f6440ed0
Dennis Wenrich <dennisfinley@gmail.com>
2023-03-18 22:12:00 -0400

    included img and stats

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+const POKE_API = 
+'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 class Pokecard extends Component {
     render() {
+        const imgSrc = `${POKE_API}${this.props.id}.png`
         return (
             <div>
+                <img src={imgSrc} alt={this.props.name}/>
                 <h1>{this.props.name}</h1>
+                <div>Type: {this.props.type}</div>
+                <div>EXP: {this.props.exp}</div>
             </div>
         )
     }
